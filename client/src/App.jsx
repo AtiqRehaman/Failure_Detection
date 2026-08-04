8c3a1e6a4de386bcad0ae8f042deb5f66249f64d
HP <ofanatryon@gmail.com>
2026-08-04 15:31:10 +0530

    routed to dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
           <Route path="/submit" element={<ProjectSubmission />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/analysis/:id" element={<ProjectAnalysis />} />
